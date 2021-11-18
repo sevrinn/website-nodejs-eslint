4476cc446519b935996f6c6e385860c7cee38d57
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 12:53:36 -0800

    set up ejs to read from views dir

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require("express")
+const express = require('express')
 const path = require("path")
 
 const app = express()
 
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "./static")))
+app.set(('view engine', 'ejs'))
+app.set('view', path.join(__dirname, './views'))
+
+app.use(express.static(path.join(__dirname, './static')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './static/index.html'))
