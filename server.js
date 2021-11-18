b13128c10e36b979d17e4640863134c04a043b5d
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-18 15:13:48 -0800

    added modular rroute to index page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require("path")
+const routes = require("./routes")
 
 const app = express()
 
@@ -9,14 +10,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
 
 app.use(express.static(path.join(__dirname, './static')))
-
-app.get('/', (req, res) => {
-  res.render('pages/index', { pageTitle: 'Welcome'})
-})
-
-app.get('/speakers', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/speakers.html'))
-})
+app.use('/', routes())
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`)
