5b4a39e35cc0b7245dd9714d805aa71785e651d9
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-19 11:13:32 -0800

    played with cookie-session but ultimately deleted visit-counter test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require("path")
+const cookieSession = require('cookie-session')
 const routes = require("./routes")
 
 const FeedbackService = require('./services/FeedbackService')
@@ -12,12 +13,20 @@ const app = express()
 
 const port = 3000
 
+app.set('trust proxy', 1)
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Ghdur687399s7w', 'hhjjdf89s866799'],
+  }))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
 
 app.use(express.static(path.join(__dirname, './static')))
 
-//
+
 app.use('/', routes({
   feedbackService,
   speakersService
