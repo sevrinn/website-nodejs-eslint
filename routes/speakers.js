16d64607f318fc8ed39f05dbffe76cc9ee683dc3
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-21 06:29:39 -0800

    added async get req for template for speakers

--- routes/speakers.js
+++ routes/speakers.js
@@ -7,8 +7,8 @@ module.exports = (params) => {
   const {speakersService} = params;
  
   router.get('/', async (req, res) => {
-    const speakers = await speakersService.getList();
-    return res.json(speakers)
+    const speakers = await speakersService.getList()
+    res.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers})
   })
 
   router.get('/:shortname', (req, res) => res.send(`Detail page of ${req.params.shortname}`))
